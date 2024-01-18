6c426c9d01b95e38f64e8bbfe0295761a7d720ce
Valentin Ballesteros <thenasky@gmail.com>
2024-01-18 02:40:06 -0300

    fucking cors

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -16,6 +16,12 @@ const corsMiddleware = cors({
 
 export default async function handler(req, res) {
    await corsMiddleware(req, res);
+   await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
    if (req.method === "POST") {
       const form = new IncomingForm();
 
